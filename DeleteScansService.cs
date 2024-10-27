63cf34019dba637788b21091374aa689fd13e16c
Tim Sonner <66705347+timsonner@users.noreply.github.com>
2024-10-27 16:56:47 -0700

    initial commit

---
+++ DeleteScansService.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Diagnostics;
+using System.ServiceProcess;
+using System.IO;
+
+public partial class DeleteScansService : ServiceBase
+{
+    private EventLog eventLog;
+
+    public DeleteScansService()
+    {
+        // References manually defined method
+        InitializeComponent(); 
+        
+        eventLog = new EventLog();
+
+        if (!EventLog.SourceExists("DeleteScansService"))
+        {
+            EventLog.CreateEventSource("DeleteScansService", "Application");
+        }
+
+        eventLog.Source = "DeleteScansService";
+        eventLog.Log = "Application";
+    }
+
+    private void InitializeComponent()
+    {
+        // Initialize service's components
+        this.ServiceName = "DeleteScansService";
+        this.CanStop = true;
+        this.CanPauseAndContinue = false;
+        this.AutoLog = true;
+    }
+
+    protected override void OnStart(string[] args)
+    {
+        // Start the service
+        try
+        {
+            string scansPath = @"C:\ProgramData\Microsoft\Windows Defender\Scans";
+            DeleteFiles(scansPath);
+        }
+        catch (Exception ex)
+        {
+            eventLog.WriteEntry("Error in DeleteScansService: " + ex.Message, EventLogEntryType.Error);
+        }
+    }
+
+    private void DeleteFiles(string path)
+    {
+        if (Directory.Exists(path))
+        {
+            DirectoryInfo di = new DirectoryInfo(path);
+            foreach (var file in di.GetFiles())
+            {
+                try
+                {
+                    file.Delete();
+                    eventLog.WriteEntry("Deleted file: " + file.FullName, EventLogEntryType.Information);
+                }
+                catch (Exception ex)
+                {
+                    eventLog.WriteEntry("Failed to delete file: " + file.FullName + " - " + ex.Message, EventLogEntryType.Error);
+                }
+            }
+            foreach (var dir in di.GetDirectories())
+            {
+                try
+                {
+                    dir.Delete(true);
+                    eventLog.WriteEntry("Deleted directory: " + dir.FullName, EventLogEntryType.Information);
+                }
+                catch (Exception ex)
+                {
+                    eventLog.WriteEntry("Failed to delete directory: " + dir.FullName + " - " + ex.Message, EventLogEntryType.Error);
+                }
+            }
+        }
+    }
+
+    protected override void OnStop()
+    {
+        // Clean up resources if needed
+    }
+}
---
+++ ProjectInstaller.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+using System.Configuration.Install;
+using System.ServiceProcess;
+
+[RunInstaller(true)]
+public class DeleteScansServiceInstaller : Installer
+{
+    public DeleteScansServiceInstaller()
+    {
+        ServiceProcessInstaller processInstaller = new ServiceProcessInstaller();
+        ServiceInstaller serviceInstaller = new ServiceInstaller();
+
+        // Set the service account type
+        processInstaller.Account = System.ServiceProcess.ServiceAccount.LocalSystem;
+
+        // Set the service properties
+        serviceInstaller.StartType = ServiceStartMode.Automatic;
+        serviceInstaller.ServiceName = "DeleteScansService";
+
+        // Add the installers to the collection
+        Installers.Add(processInstaller);
+        Installers.Add(serviceInstaller);
+    }
+}
